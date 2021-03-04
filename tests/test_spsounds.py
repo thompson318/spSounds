17e9555e006e99807d256426be5dc85bdc4d2eb4
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-03-04 21:22:27 +0000

    Testing stuff

--- tests/test_spsounds.py
+++ tests/test_spsounds.py
@@ -3,24 +3,10 @@
 """spSounds tests"""
 
 from spsounds.ui.spsounds_demo import run_demo
-from spsounds.algorithms import addition, multiplication
+from spsounds.midi import sounds
 
 # Pytest style
 def test_using_pytest_spsounds():
     """First test"""
     #pylint:disable=invalid-name
-    x = 1
-    y = 2
-    verbose = False
-    multiply = False
-
-    expected_answer = 3
-    assert run_demo(x, y, multiply, verbose) == expected_answer
-
-def test_addition():
-    """ Test addition """
-    assert addition.add_two_numbers(1, 2) == 3
-
-def test_multiplication():
-    """ Test multiplication """
-    assert multiplication.multiply_two_numbers(2, 2) == 4
+    assert run_demo(args = "nothing")
